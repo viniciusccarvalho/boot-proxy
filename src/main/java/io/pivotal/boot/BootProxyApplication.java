d6e8a95e024ff1056345eb97beea527e13eeb7ed
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-12-23 20:28:35 -0500

    initial commit

---
+++ src/main/java/io/pivotal/boot/BootProxyApplication.java
@@ -0,0 +1,16 @@
+package io.pivotal.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootProxyApplication.class, args);
+    }
+
+
+
+
+}
